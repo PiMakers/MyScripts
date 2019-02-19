b08c796934247d7e30a59e9f3b0dd6e53307cf39
PiMakers <PiMakers@gmail.com>
2019-02-19 14:04:35 +0100

    Bootstrap.sh resize

--- Rpi/Bootstrap.sh
+++ Rpi/Bootstrap.sh
@@ -6,17 +6,27 @@
 #!/bin/bash
 
 ##### dependency 18.04 sudo apt install qemu-user-static
+# qemu-img resize -f raw /home/pimaker/Downloads/2018-11-13-raspbian-stretch.img +2G
  
 set -e
 
-check_root () {
-if [ $EUID ]; then
-	echo "this script must be run as root"
-	echo ""
-	echo "usage:"
-	echo "sudo "$0
-	exit 1
-fi
+check_root() {
+    # Must be root to install the hotspot
+    echo ":::"
+    if [[ $EUID -eq 0 ]];then
+        echo "::: You are root - OK"
+    else
+        echo "::: sudo will be used for the install."
+        # Check if it is actually installed
+        # If it isn't, exit because the install cannot complete
+        if [[ $(dpkg-query -s sudo) ]];then
+            export SUDO="sudo"
+            export SUDOE="sudo -E"
+        else
+            echo "::: Please install sudo or run this as root."
+            exit 1
+        fi
+    fi
 }
 
 export LC_LOCAL=C
@@ -35,8 +45,8 @@ fi
 
 ROOT=$( cd "$(dirname "$0")" ; pwd -P )
 echo -e " ROOT = $ROOT\n PROGNAME = $PROGNAME\n PROGDIR = $PROGDIR\n ARGS = $ARGS\n"
-export TEMP_FOLDER=/tmp
-export RPI_FS=${TEMP_FOLDER}/rpifs
+export TEMP_DIR=/tmp
+export RPI_FS=${TEMP_DIR}/rpifs
 export RPI_ROOT=${RPI_FS}/rootfs
 export RPI_BOOT=${RPI_FS}/boot
 
@@ -62,14 +72,17 @@ curl -LJ https://downloads.raspberrypi.org/raspbian_${RASPBIAN_TYPE}latest -O ||
 get_latest_image() {
 cd ${HOME}/Downloads
 if [ ! -f $LATEST_VERSION.img ]; then
- echo "$LATEST_VERSION.img not found.\nSearching for $LATEST_VERSION.zip ..."
+ echo -e "$LATEST_VERSION.img not found.\nSearching for $LATEST_VERSION.zip ..."
  if [ ! -f $LATEST_VERSION.zip ]; then
     echo "$LATEST_VERSION.zip not found. Downloading ..."
     download_latest_raspbian || echo "Error download latest raspbian"
  else echo "$LATEST_VERSION.zip found!"
  fi
  unzip $LATEST_VERSION.zip || (echo "Error unzip latest raspbian" && exit 101)
+else 
+  echo "$LATEST_VERSION.img found!"
 fi
+
 return 0
 }
 
@@ -77,26 +90,29 @@ return 0
 
 #umount ${RPI_BOOT} ${RPI_ROOT} || true
 mount_latest_img() {
-LOOP_DEVICE=$(sudo losetup -f)
-[[ ! -z "$1" ]] && err=$(sudo losetup $LOOP_DEVICE $1) || (echo "$err" && return 1)
-sudo partprobe $LOOP_DEVICE 	|| echo "error partprobe $LOOP_DEVICE "
-
+export LOOP_DEVICE=$(sudo losetup -f)
+[[ ! -z "$1" ]] && err=$(${SUDO} losetup $LOOP_DEVICE $1) || (echo "$err" && return 1)
+${SUDO} partprobe $LOOP_DEVICE 	|| echo "error partprobe $LOOP_DEVICE "
+start_sector=$(${SUDO} fdisk -l ${LOOP_DEVICE} | sed "/${LOOP_DEVICE//[\/]/\\/}p2/!d; s/  */ /g; s/[^ ]*. //; s/ .*//") #| cut -d " " -f 2
+#start_sector=$(${SUDO} fdisk -l ${LOOP_DEVICE} | sed "/${LOOP_DEVICE//[\/]/\\/}p2/!d; s/*[ ]//g" | cut -d " " -f 2)
+echo "start_sector =${start_sector}"
 mkdir -p ${RPI_BOOT} ${RPI_ROOT}   || echo "error create ${RPI_BOOT} "
-sudo mount -n ${LOOP_DEVICE}p1 ${RPI_BOOT} || echo "error mounting ${RPI_BOOT}" 
-sudo mount -n ${LOOP_DEVICE}p2 ${RPI_ROOT} || echo "error mounting ${RPI_ROOT}" 
+#${SUDO} mount -n ${LOOP_DEVICE}p1 ${RPI_BOOT} || echo "error mounting ${RPI_BOOT}" 
+#${SUDO} mount -n ${LOOP_DEVICE}p2 ${RPI_ROOT} || echo "error mounting ${RPI_ROOT}"
+${SUDO} losetup -d $LOOP_DEVICE
 }
 
 clean_up() {
 echo "CleaningUp ...."
 
 # umount everything if needed
-err=$(sudo umount -v ${RPI_ROOT}/{sys,proc,dev/pts,dev,/boot,/mnt/LinuxData} 2>&1) || echo -e "$err"
+err=$(${SUDO} umount -v ${RPI_ROOT}/{sys,proc,dev/pts,dev,boot} 2>&1) || echo -e "$err"
 dirs="$RPI_ROOT $RPI_BOOT ${RPI_FS}"
 for dir in $dirs; do
 [[ "$(ls $dir)" != "" ]] && echo "dir not empty" && \
 for x in $(mount | grep $dir | sed 's/type.*//; s/.*on //'); do umount -lv "$x";done || \
  echo "$dir now empty removing..."
-err=$(sudo rm -r $dir 2>/dev/null)|| ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} rm -r $dir 2>/dev/null)|| ( echo "ERROR: $err" && return 101)
 done
 echo "clean_up: Bye-bye...."
 }
@@ -104,45 +120,49 @@ echo "clean_up: Bye-bye...."
 chroot_raspbian () {
 trap clean_up EXIT 2 SIGINT
 # sleep 100
-err=$(sudo cp /usr/bin/qemu-arm-static ${RPI_ROOT}/usr/bin/ 2>&1) 	|| ( echo "ERROR: $err" && return 101)
-#sudo cp ${RPI_ROOT}/etc/resolv.conf 
-err=$(sudo cp /etc/resolv.conf ${RPI_ROOT}/etc/resolv.conf 2>&1) || ( echo "ERROR: $err" && return 101)
-#err=$(sudo mkdir -p ${RPI_ROOT}/mnt/LinuxData 2>&1) || ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} cp /usr/bin/qemu-arm-static ${RPI_ROOT}/usr/bin/ 2>&1) 	|| ( echo "ERROR: $err" && return 101)
+#${SUDO} cp ${RPI_ROOT}/etc/resolv.conf 
+err=$(${SUDO} cp /etc/resolv.conf ${RPI_ROOT}/etc/resolv.conf 2>&1) || ( echo "ERROR: $err" && return 101)
+#err=$(${SUDO} mkdir -p ${RPI_ROOT}/mnt/LinuxData 2>&1) || ( echo "ERROR: $err" && return 101)
 
 cd ${RPI_ROOT}
 
-err=$(sudo mount --bind /proc ${RPI_ROOT}/proc/ 2>&1) 	|| ( echo "ERROR: $err" && return 101)
-err=$(sudo mount --bind /dev ${RPI_ROOT}/dev/ 2>&1) 		|| ( echo "ERROR: $err" && return 101)
-err=$(sudo mount --bind /dev/pts ${RPI_ROOT}/dev/pts 2>&1) 	|| ( echo "ERROR: $err" && return 101)
-err=$(sudo mount --bind /sys ${RPI_ROOT}/sys/ 2>&1) 		|| ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} mount --bind /proc ${RPI_ROOT}/proc/ 2>&1) 	|| ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} mount --bind /dev ${RPI_ROOT}/dev/ 2>&1) 		|| ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} mount --bind /dev/pts ${RPI_ROOT}/dev/pts 2>&1) 	|| ( echo "ERROR: $err" && return 101)
+err=$(${SUDO} mount --bind /sys ${RPI_ROOT}/sys/ 2>&1) 		|| ( echo "ERROR: $err" && return 101)
 
-err=$(sudo mount --bind ${RPI_BOOT} ${RPI_ROOT}/boot/ 2>&1) 			|| ( echo "ERROR: $err" && return 101)
-#err=$(sudo mount --bind /mnt/LinuxData ${RPI_ROOT}/mnt/LinuxData 2>&1) 	|| ( echo "ERROR: $err" && return 101)
-# cmd=/mnt/LinuxData/Scripts/Rpi/setupNew.sh
-# chroot . $cmd 						|| echo "ERROR: chroot"
-sudo chroot . "apt update"  || ( echo "ERROR: chroot" && clean_up )
+err=$(${SUDO} mount --bind ${RPI_BOOT} ${RPI_ROOT}/boot/ 2>&1) 			|| ( echo "ERROR: $err" && return 101)
+#err=$(${SUDO} mount --bind /mnt/LinuxData ${RPI_ROOT}/mnt/LinuxData 2>&1) 	|| ( echo "ERROR: $err" && return 101)
+#cmd=  #/mnt/LinuxData/Scripts/Rpi/setupNew.sh
+${SUDO} chroot .    || echo "ERROR: chroot"
+# ${SUDO} chroot . || ( echo "ERROR: chroot" && clean_up )
 
-err=$(sudo umount -v ${RPI_ROOT}/{sys,proc,dev/pts,dev,/boot,/mnt/LinuxData} 2>&1) || echo -e "$err"
-# sudo umount ${RPI_ROOT}/mnt/LinuxData  || echo "error LinuxData"
-# sudo umount ${RPI_ROOT}/boot/  || echo "error uBoot"
+err=$(${SUDO} umount -v ${RPI_ROOT}/{sys,proc,dev/pts,dev,boot} 2>&1) || echo -e "$err"
+# ${SUDO} umount ${RPI_ROOT}/mnt/LinuxData  || echo "error LinuxData"
+# ${SUDO} umount ${RPI_ROOT}/boot/  || echo "error uBoot"
 
-sudo rm ${RPI_ROOT}/usr/bin/qemu-arm-static || echo "error remove qemu-arm-static"
+${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static || echo "error remove qemu-arm-static"
+${SUDO} umount -lv ${RPI_BOOT} || echo "could not umount RPI_BOOT ($RPI_BOOT)"
+${SUDO} umount -lv ${RPI_ROOT} || echo "could not umount $RPI_ROOT"
+${SUDO} losetup -d $LOOP_DEVICE || echo "could not umount losetup -d $LOOP_DEVICE"
 echo "chroot_raspbian: Bye-bye...."
 }
 
 
 
 my_exit () {
-sudo umount -lv ${RPI_BOOT} || echo "could not umount RPI_BOOT ($RPI_BOOT)"
-sudo umount -lv ${RPI_ROOT} || echo "could not umount $RPI_ROOT"
-sudo losetup -d $LOOP_DEVICE || echo "could not umount losetup -d $LOOP_DEVICE"
+${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static || echo "error remove qemu-arm-static"    
+${SUDO} umount -lv ${RPI_BOOT} || echo "could not umount RPI_BOOT ($RPI_BOOT)"
+${SUDO} umount -lv ${RPI_ROOT} || echo "could not umount $RPI_ROOT"
+${SUDO} losetup -d $LOOP_DEVICE || echo "could not umount losetup -d $LOOP_DEVICE"
 }
 
+check_root
 latest_version
 get_latest_image
 mount_latest_img $LATEST_VERSION.img || echo "error mounting $LATEST_VERSION.img"
-#chroot_raspbian
-#my_exit
+#chroot_raspbian || my_exit
 
 
 echo "exited normally" && exit 0
