ffab843cf3f0d3b55fa817384b6df460da2277f0
PiMakers <PiMakers@gmail.com>
2019-07-10 11:04:20 +0200

    utils_update

--- Rpi/Bootstrap.sh
+++ Rpi/Bootstrap.sh
@@ -146,14 +146,18 @@ mount_image() {
 clean_up() {
     echo "CleaningUp ...."
     # umount everything if needed
-    ${SUDO} umount -v ${RPI_ROOT}/{sys,proc,dev/pts,dev,boot}
+    USER=${OLD_USER}
+    HOME=${OLD_HOME}
+
+    ${SUDO} umount -lv ${RPI_ROOT}/{mnt/LinuxData,sys,proc,dev/pts,dev,boot}
+
     dirs="${RPI_ROOT} ${RPI_BOOT} ${RPI_FS}"
     for dir in $dirs
         do
         [[ "$(ls $dir)" != "" ]] && echo "dir not empty" || \
         for x in $(mount | grep $dir | sed '/sda/d; s/type.*//; s/.*on //');
             do
-                umount -lv "$x" || echo "ERROR: cannot remove dir $x";
+                ${SUDO} umount -lv "$x" || echo "ERROR: cannot remove dir $x";
             done || \
             echo "$dir now empty removing..."
             ${SUDO} rm -r $dir
@@ -163,15 +167,17 @@ clean_up() {
 }
 
 chroot_raspbian () {
-    trap clean_up SIGINT 2
+    #trap clean_up SIGINT 2
+    trap my_exit SIGINT 2
     [ -z $1 ] || LATEST_VERSION=$1
     echo "LATEST_VERSION=$LATEST_VERSION.img" && \
     mount_image ${LATEST_VERSION}.img
     # sleep 100
     ${SUDO} cp /usr/bin/qemu-arm-static ${RPI_ROOT}/usr/bin/qemu-arm-static
     ${SUDO} cp /etc/resolv.conf ${RPI_ROOT}/etc/resolv.conf
-    ${SUDO} cp -r /mnt/LinuxData/OF/myGitHub/MyScripts ${RPI_ROOT}/home/pi/
-#    ${SUDO} touch /boot/PiMaker.log
+    ${SUDO} mv ${RPI_ROOT}/etc/ld.so.preload ${RPI_ROOT}/etc/ld.so.preload.orig
+    # ${SUDO} cp -r /mnt/LinuxData/OF/myGitHub/MyScripts ${RPI_ROOT}/home/pi/
+    # ${SUDO} touch /boot/PiMaker.log
     
     cd ${RPI_ROOT}
 
@@ -192,21 +198,8 @@ chroot_raspbian () {
     HOME=/home/${USER}
     #err=$(${SUDO} chroot . $cmd) || echo "ERROR:  $err !!!!!!!!!!!"  
     ${SUDO} chroot --userspec=pi:root . || echo "ERROR:  $err !!!!!!!!!!!" #$cmd || echo "ERROR:  $err !!!!!!!!!!!"
-    USER=${OLD_USER}
-    HOME=/home/${OLD_USER}
-    #cd
-    ${SUDO} umount -lv ${RPI_ROOT}/{sys,proc,dev/pts,dev,boot}
-    err=$(${SUDO} umount -lv ${RPI_ROOT}/mnt/LinuxData)  || echo "$err"
-
-    ${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static
-    ${SUDO} rm ${RPI_ROOT}/etc/resolv.conf 
-    ${SUDO} rm -r ${RPI_ROOT}/home/pi/MyScripts
-    #${SUDO} rm ${RPI_ROOT}/home/pi/temp.sh
-    sync
-    ${SUDO} umount -lv ${RPI_BOOT}
-    ${SUDO} umount -lv ${RPI_ROOT}
-    ${SUDO} losetup -v -d $LOOP_DEVICE
-    err=$(${SUDO} rm -r ${RPI_FS}) || echo "$err"
+    
+    my_exit
     
     echo "chroot_raspbian: Bye-bye...."
 
@@ -222,13 +215,40 @@ chroot_raspbian () {
 }
 
 my_exit () {
-echo "MyExit....."
-${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static || echo "error remove qemu-arm-static"    
-${SUDO} umount -lv ${RPI_BOOT} || echo "could not umount RPI_BOOT ($RPI_BOOT)"
-${SUDO} umount -lv ${RPI_ROOT} || echo "could not umount $RPI_ROOT"
-${SUDO} losetup -d $LOOP_DEVICE || echo "could not umount losetup -d $LOOP_DEVICE"
-echo "MyExit happend!!!!"
-exit 101
+  echo "MyExit....."
+    # ******************
+    USER=${OLD_USER}
+    HOME=${OLD_HOME}
+    #cd
+    err=$(${SUDO} umount -lv ${RPI_ROOT}/{mnt/LinuxData/,sys,proc,dev/pts,dev,boot})  || echo "$err"
+
+    err=$(${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static) || echo "$err"
+    err=$(${SUDO} rm ${RPI_ROOT}/etc/resolv.conf) || echo "$err"
+    [ -f ${RPI_ROOT}/etc/ld.so.preload.orig ] && ${SUDO} mv ${RPI_ROOT}/etc/ld.so.preload.orig ${RPI_ROOT}/etc/ld.so.preload
+
+    sync
+    ${SUDO} umount -lv ${RPI_BOOT}
+    ${SUDO} umount -lv ${RPI_ROOT}
+    ${SUDO} losetup -v -d $LOOP_DEVICE
+    err=$(${SUDO} rm -r ${RPI_FS}) || echo "$err"
+    
+    read -p "Save changes? (y/any)" IMG_NAME
+    [[ ${IMG_NAME} != "y" ]] && ${SUDO} rm ${TEMP_DIR}/${LATEST_VERSION}.img && exit 101
+    read -p "Type a name (extension) for new img: " IMG_NAME
+#    mv  ${HOME}/Downloads/${LATEST_VERSION}.img "${LATEST_VERSION}_${IMG_NAME}.img"
+    ${SUDO} mv  ${TEMP_DIR}/${LATEST_VERSION}.img "/mnt/LinuxData/SavedDiscImges/${LATEST_VERSION}${IMG_NAME}.img"
+    echo "Image saved as /mnt/LinuxData/SavedDiscImges/${LATEST_VERSION}${IMG_NAME}.img"
+    #chown ${USER}:${USER} ${NEW_IMG_NAME}
+    #read -p "Write SD card ? (y any)" IMG_NAME
+    #[ ${IMG_NAME} == "y"] && sudo dd bs=1M if=$LATEST_VERSION_${IMG_NAME}.img of=/dev/sdx
+
+
+    ${SUDO} rm ${RPI_ROOT}/usr/bin/qemu-arm-static || echo "error remove qemu-arm-static"    
+    ${SUDO} umount -lv ${RPI_BOOT} || echo "could not umount RPI_BOOT ($RPI_BOOT)"
+    ${SUDO} umount -lv ${RPI_ROOT} || echo "could not umount $RPI_ROOT"
+    ${SUDO} losetup -d $LOOP_DEVICE || echo "could not umount losetup -d $LOOP_DEVICE"
+    echo "MyExit happend!!!!"
+#  exit 101
 }
 
 check_root
@@ -244,51 +264,32 @@ echo "exited normally"
 # exit 0
 
 more() {
-# enable ssh
-touch ${RPI_BOOT}/ssh
 
-#############
-# Silent Boot
-#############
-# 1. remove: Logo, blinking cursor, add:loglevel=3
-grep -q 'logo.nologo' ${RPI_BOOT}/cmdline.txt || sed -i 's/$/ logo.nologo/' ${RPI_BOOT}/cmdline.txt
-grep -q 'vt.global_cursor_default=0' ${RPI_BOOT}/cmdline.txt || sed -i 's/$/ vt.global_cursor_default=0/' ${RPI_BOOT}/cmdline.txt
-grep -q 'loglevel=' ${RPI_BOOT}/cmdline.txt || sed -i 's/$/ loglevel=3/' ${RPI_BOOT}/cmdline.txt
+  sed -i '/LinuxData/d' ${RPI_ROOT}/etc/fstab
+  echo "#192.168.0.13:/LinuxData /mnt/LinuxData nfs rw  0  0" >> ${RPI_ROOT}/etc/fstab
+  echo "//192.168.0.13/LinuxData /mnt/LinuxData cifs username=pimaker,password=raspi,vers=2.0,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0" >> ${RPI_ROOT}/etc/fstab
+  sed -i 'export LC_ALL=C' ${RPI_ROOT}/home/pi/.bashrc
+  echo "export LC_ALL=C" >> ${RPI_ROOT}/home/pi/.bashrc
+  cat > ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant-wlan0.conf << EOF
+  country=HU
+  ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+  update_config=1
 
-# Remove Rainbow Screen
-grep -q '^disable_splash' ${RPI_BOOT}/config.txt || \
-grep -q '# disable_splash' ${RPI_BOOT}/config.txt && \
-sed -i '/^# disable_splash/ s/# //' ${RPI_BOOT}/config.txt || \
-( echo -e "\n# Disable rainbow image at boot\t\t#PubHub" >> ${RPI_BOOT}/config.txt && \
-echo -e "disable_splash=1\t\t\t#PubHub" >> ${RPI_BOOT}/config.txt )
-
-
-
-sed -i '/LinuxData/d' ${RPI_ROOT}/etc/fstab
-echo "#192.168.0.13:/LinuxData /mnt/LinuxData nfs rw  0  0" >> ${RPI_ROOT}/etc/fstab
-echo "//192.168.0.13/LinuxData /mnt/LinuxData cifs username=pimaker,password=raspi,vers=2.0,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0" >> ${RPI_ROOT}/etc/fstab
-sed -i 'export LC_ALL=C' ${RPI_ROOT}/home/pi/.bashrc
-echo "export LC_ALL=C" >> ${RPI_ROOT}/home/pi/.bashrc
-cat > ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant-wlan0.conf << EOF
-country=HU
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
-
-network={
-    scan_ssid=1
-    ssid="${CLIENT_SSID}"
-    psk="${CLIENT_PASSPHRASE}"
-#    key_mgmt=WPA-PSK
-#    priority=99
-}
+  network={
+      scan_ssid=1
+      ssid="${CLIENT_SSID}"
+      psk="${CLIENT_PASSPHRASE}"
+  #    key_mgmt=WPA-PSK
+  #    priority=99
+  }
 EOF
-[ -e ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant.conf ] && rm ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant.conf
+  [ -e ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant.conf ] && rm ${RPI_ROOT}/etc/wpa_supplicant/wpa_supplicant.conf
 
-sync
-sleep 5
-umount ${RPI_BOOT} ${RPI_ROOT} && sudo losetup -d $LOOP_DEVICE
-NEW_IMG_NAME=modded_$(date +"%H%M%S")${IMG_NAME}
-mv  ${IMG_NAME} ${NEW_IMG_NAME}
-chown ${USER}:${USER} ${NEW_IMG_NAME}
+  sync
+  sleep 5
+  umount ${RPI_BOOT} ${RPI_ROOT} && sudo losetup -d $LOOP_DEVICE
+  NEW_IMG_NAME=modded_$(date +"%H%M%S")${IMG_NAME}
+  mv  ${IMG_NAME} ${NEW_IMG_NAME}
+  chown ${USER}:${USER} ${NEW_IMG_NAME}
 
 }
