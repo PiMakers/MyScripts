361aca3f3d5ddf4ae995bc7b8bacfc3e0ddf92d4
PiMakers <PiMakers@gmail.com>
2020-10-04 09:22:55 +0200

    tmp install for T.I.

--- Net/Installnodejs.sh
+++ Net/Installnodejs.sh
@@ -18,18 +18,21 @@ detectSyystem () {
 
 tmp=$(sed '/^ID=/!d; s/^ID=//'  /etc/os-release) && [ "$tmp" = "raspbian" ] || ( echo -e "RunThisScript on raspbian" && exit 111 )
 
-ARCH=armv6l
-VERSION=latest ## should be etc: latest,  v7.2.1, or latest-v7.x
+BASE_URL=https://nodejs.org/dist
+ARCH=armv6l # linux: armv6l | armv7l | arm64 | x64 | x86 | ppc64le | s390x
+VERSION=latest-v8.x ## should be etc: latest,  v7.2.1, or latest-v7.x
+#SUBVERSION=8.x
+#DL_URL=${BASE_URL}/${VERSION}${SUBVERSION#^*/-}
 
 NODE_JS=$(curl -sL https://nodejs.org/dist/${VERSION} | grep "${ARCH}.tar.xz" | cut -d'"' -f2)
 
-wget https://nodejs.org/dist/${VERSION}/${NODE_JS} -O /tmp/${NODE_JS}
+wget ${BASE_URL}/${VERSION}/${NODE_JS} -O /tmp/${NODE_JS}
 
 sudo tar -xvf /tmp/${NODE_JS} -C /tmp/
 
 ([ -f /opt/nodejs ] && sudo rm -r /opt/nodejs && echo "removing old nodejs...") || \
 
-sudo mv /tmp/${NODE_JS:: -7} /opt/nodejs
+sudo mv /tmp/${NODE_JS:: -7} /opt/nodejs${VERSION/latest-/}
 
 # Remove existing symlinks
 sudo unlink /usr/bin/node || true
@@ -42,11 +45,13 @@ sudo unlink /sbin/npm || true
 sudo unlink /usr/local/bin/npm || true
 
 # Create symlinks to node && npm
-sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
-sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
-sudo ln -s /opt/nodejs/bin/node /sbin/node;
-sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node;
-sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
-sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
-sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
-sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/node /usr/bin/node;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/node /usr/sbin/node;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/node /sbin/node;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/node /usr/local/bin/node;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/npm /usr/bin/npm;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/npm /usr/sbin/npm;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/npm /sbin/npm;
+sudo ln -s /opt/nodejs${VERSION/latest-/}/bin/npm /usr/local/bin/npm;
+
+# curl https://raw.githubusercontent.com/PiMakers/MyScripts/edit/Rpi/setUp/setupTI.sh | bash -s
