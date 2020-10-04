b6ef5067c9a4d4498b004417c5e321859c62fd72
PiMakers <PiMakers@gmail.com>
2020-10-04 09:57:39 +0200

    test

--- Rpi/setUp/setupTI.sh
+++ Rpi/setUp/setupTI.sh
@@ -39,6 +39,7 @@ check_root() {
 set_defaults() {
     export LC_ALL=C
     REMOTE_GIT_BASE_URL=https://raw.githubusercontent.com/PiMakers/MyScripts/edit
+    echo "REMOTE_GIT_BASE_URL = ${REMOTE_GIT_BASE_URL}..."
     #"${2}"
     WLAN_AP=wlan0
     AP_SSID="T.I.Remote"
