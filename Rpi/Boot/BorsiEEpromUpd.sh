b5e74b72eb3d5f6990d6997fed8f10b4764a0ca9
pimaker <PiMakers@gmail.com>
2021-07-28 21:21:40 +0200

    upd

--- Rpi/Boot/BorsiEEpromUpd.sh
+++ Rpi/Boot/BorsiEEpromUpd.sh
@@ -4,14 +4,14 @@
 
 TMP_DIR=/tmp
 
-    cat << EOF | sed 's/^.\{4\}//' | ${SUDO} tee ${TMP_DIR}/boot.conf
+    cat << EOF | sed 's/^.\{8\}//' | ${SUDO} tee ${TMP_DIR}/boot.conf
         [all]
         BOOT_UART=0
         WAKE_ON_GPIO=1
         POWER_OFF_ON_HALT=0
 
-        # Try  Network -> SD- > Loop
-        BOOT_ORDER=0xf12
+        # Try  Network -> MSD/USB -> SD- > Loop
+        BOOT_ORDER=0xf412
 
         # Set to 0 to prevent bootloader updates from USB/Network boot
         # For remote units EEPROM hardware write protection should be used.
@@ -19,6 +19,17 @@ TMP_DIR=/tmp
 
         # default=0 silent=1
         DISABLE_HDMI=1
+
+        #DHCP_TIMEOUT=45000
+        #DHCP_REQ_TIMEOUT=4000
+        #TFTP_FILE_TIMEOUT=30000
+        #TFTP_IP=
+        #TFTP_PREFIX=0
+        SD_BOOT_MAX_RETRIES=1
+        NET_BOOT_MAX_RETRIES=2
+
+        [none]
+        FREEZE_VERSION=0
 EOF
 
 CM4_ENABLE_RPI_EEPROM_UPDATE=1 sudo -E rpi-eeprom-config --apply ${TMP_DIR}/boot.conf && RES="$?"
--- Rpi/Boot/nfsBootLibreELEC.sh
+++ Rpi/Boot/nfsBootLibreELEC.sh
@@ -1,5 +1,7 @@
 ## https://libreelec.wiki/configuration/network-boot
 ## https://forum.libreelec.tv/thread/20163-rpi4-gpio-using-in-libreelec/
+## https://www.raspberrypi.org/forums/viewtopic.php?t=134133 /net conf!!!!
+## https://forum.kodi.tv/showthread.php?tid=260817
 
 # sudo mount 192.168.1.20:/mnt/LinuxData/OF /mnt/LinuxData/OF
 #!/bin/bash
@@ -20,7 +22,7 @@ DHCP=1
         HOST_IP=10.0.0.1
     fi
 
-IMG=${IMG_DIR}/LibreELEC-RPi4.arm-9.95.4.img
+IMG=${IMG_DIR}/LibreELEC-RPi4.arm-9.97.1.img
 #LibreELEC-RPi4.arm-9.2.6.img
 
 get_img(){
@@ -46,7 +48,7 @@ get_img(){
 }
 
 resizeImage() {
-    local COUNT=4096
+    local COUNT=6144
     ${SUDO} bash -c "dd if=/dev/zero bs=1M count=${COUNT} >> ${IMG}"
 }
 
@@ -60,7 +62,8 @@ mountLE() {
     # read -p "Press ENTER to continue..."
 }
 
-prepare() {
+# dtoverlay=dwc2,dr_mode=host
+netBoot() {
     ${SUDO} mkdir -pv ${STORAGE_DIR}
     echo "boot=NFS=${HOST_IP}:${TFTP_DIR} morequiet disk=NFS=${HOST_IP}:${STORAGE_DIR} rw ip=dhcp rootwait quiet systemd.show_status=0" | \
         ${SUDO} tee ${TFTP_DIR}/cmdline.nfsboot.LE
@@ -85,8 +88,10 @@ prepare() {
     else
         DHCP_OPT="--dhcp-range=${HOST_IP%.*}.2,${HOST_IP%.*}.10,12h --listen-address=127.0.0.1,10.0.0.1 --port=5300"
     fi
-    ${SUDO} dnsmasq --enable-tftp --tftp-root=${TFTP_DIR},enp0s25 -d --pxe-service=0,"Raspberry Pi Boot" --pxe-prompt="Boot Raspberry Pi",1 \
-        --tftp-unique-root=mac --dhcp-reply-delay=1 ${DHCP_OPT} # --dhcp-host=e4:5f:01:1f:b7:54,set:piserver tag:piserver,
+    gnome-terminal -t "tftpBoot" -- ${SUDO} dnsmasq --enable-tftp --tftp-root=${TFTP_DIR},enp0s25 -d --pxe-service=0,"Raspberry Pi Boot" \
+        --dhcp-reply-delay=1  ${DHCP_OPT} #--tftp-unique-root=mac --pxe-prompt="Boot Raspberry Pi",1 --dhcp-host=e4:5f:01:1f:b7:54,set:piserver tag:piserver,
+    
+    read -p "Press a key to continue.."
 }
 
 LEversion() {
@@ -95,9 +100,6 @@ LEversion() {
     sudo mount /tftpLE/SYSTEM /mnt/sqfs -t squashfs -o loop
     . /mnt/sqfs/etc/os-release
     [ "${VERSION_ID%.*}" -gt 9 ] && echo ":: Ten" || echo ":: Nine"
-    sudo umount -lv /mnt/sqfs
-    sudo rm -r /mnt/sqfs
-
 }
 
 playStartUpVideo() {
@@ -120,7 +122,7 @@ EOF
 EOF
         cat << EOF | sed 's/^.\{12\}//' | ${SUDO} tee ${STORAGE_DIR}/.kodi/addons/service.autoexec/addon.xml 1>/dev/null
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-            <addon id="service.autoexec" name="Autoexec Service" version="1.0.0" provider-name="your username">
+            <addon id="service.autoexec" name="Autoexec Service" version="1.0.0" provider-name="PiMaker">
                 <requires>
                     <import addon="xbmc.python" version="3.0.0"/>
                 </requires>
@@ -134,36 +136,45 @@ EOF
                 </extension>
             </addon>
 EOF
-# addon id="virtual.rpi-tools
+# addon id="virtual.rpi-tools"
     fi
     echo ":: VERSION_ID=${VERSION_ID}"
 }
-# ${STORAGE_DIR}/.kodi/userdata/addon_data/service.libreelec.settings/oe_settings.xml - WIZZARD + HOSTNAME!!!! KODI10
+
+ # ${STORAGE_DIR}/.kodi/userdata/addon_data/service.libreelec.settings/oe_settings.xml - WIZZARD + HOSTNAME!!!! KODI10
 disableSplash() {
     # mkdir -pv ${STORAGE_DIR}/.kodi/userdata/
+    ## disable RPI splash
+    ${SUDO} sed -i '/disable_splash/d' ${TFTP_DIR}/config.txt
+    echo "disable_splash=1" | ${SUDO} tee -a ${TFTP_DIR}/config.txt
+
     # Disable LibreELEC Splash
     echo | sudo tee /tftpLE/oemsplash.png
     cat << EOF | sed 's/^.\{8\}//' | ${SUDO} tee ${STORAGE_DIR}/.kodi/userdata/advancedsettings.xml 1>/dev/null    
         <advancedsettings version="1.0">
             <services>
                 <webserver>true</webserver>
+                <webserverport>8080</<webserverport>
                 <webserverpassword>raspi</webserverpassword>
                 <webserverusername>KODI</webserverusername>
             </services>
             <splash>false</splash>
             <cache>
                 <buffermode>1</buffermode>
-                <memorysize>139460608</memorysize>
+                <memorysize>209715200</memorysize>
                 <readfactor>20</readfactor>
             </cache>
-        </advancedsettings>    
+            <gui>
+                <smartredraw>true</smartredraw>
+            </gui>
+        </advancedsettings>
 EOF
 }
 
 wizzard() {
     sudo mkdir -pv ${STORAGE_DIR}/.kodi/userdata/addon_data/service.libreelec.settings
-    cat << EOF | sed 's/^.\{4\}//' | ${SUDO} tee ${STORAGE_DIR}/.kodi/userdata/addon_data/service.libreelec.settings/oe_settings.xm 1>/dev/null    
-    xml version="1.0" ?>
+    cat << EOF | sed 's/^.\{4\}//' | ${SUDO} tee ${STORAGE_DIR}/.kodi/userdata/addon_data/service.libreelec.settings/oe_settings.xml 1>/dev/null    
+    <?xml version="1.0" ?>
     <libreelec>
         <addon_config/>
             <settings>
@@ -180,26 +191,34 @@ wizzard() {
                         <wizard_completed>True</wizard_completed>
                 </libreelec>
             </settings>
-        </libreelec>
+    </libreelec>
 EOF
 }
 
 createSshKey() {
         HOSTNAME=$(hostname -s)
+        HOSTNAME=Borsi
         ${SUDO} mkdir -pv -m 700 ${STORAGE_DIR}/.ssh
         [ -f ~/.ssh/testkey@${HOSTNAME} ] || ${SUDO} ssh-keygen -q -N Pepe374189 -C testKey -f ~/.ssh/testkey@${HOSTNAME}
         ${SUDO} cat ~/.ssh/testkey@${HOSTNAME}.pub | ${SUDO} tee ${STORAGE_DIR}/.ssh/authorized_keys 1>/dev/null
         ${SUDO} chmod 600 ${STORAGE_DIR}/.ssh/authorized_keys
         ${SUDO} touch ${STORAGE_DIR}/.cache/services/sshd.conf
-        echo "SSH_ARGS=-o 'PasswordAuthentication no'" | ${SUDO} tee ${STORAGE_DIR}/.cache/services/sshd.conf
-        echo "SSHD_DISABLE_PW_AUTH=true" | ${SUDO} tee -a ${STORAGE_DIR}/.cache/services/sshd.conf
+        echo "SSH_ARGS=-o 'PasswordAuthentication yes'" | ${SUDO} tee ${STORAGE_DIR}/.cache/services/sshd.conf
+        echo "SSHD_DISABLE_PW_AUTH=false" | ${SUDO} tee -a ${STORAGE_DIR}/.cache/services/sshd.conf
 }
 
 skinHack() {
-    if [ -d ${STORAGE_DIR}/.kodi/addons/skin.estuary_Borsi ]; then
-      cp -r /usr/share/kodi/addons/skin.estuary ${STORAGE_DIR}/.kodi/addons/skin.estuary_Borsi
-      sed -i 's/skin.estuary/skin.estuary_Borsi/g;s/Estuary/Estuary_Borsi/g; s/phil65, Ichabod Fletchman/PiMaker(Hollos)/' \
-            .kodi/addons/skin.estuary_Borsi/addon.xml
+    if [ ! -d ${STORAGE_DIR}/.kodi/addons/skin.estuary_Borsi ]; then
+        if [ -d /mnt/sqfs ];then
+            ${SUDO} cp -r /mnt/sqfs/usr/share/kodi/addons/skin.estuary ${STORAGE_DIR}/.kodi/addons/skin.estuary_Borsi
+            ${SUDO} sed -i 's/skin.estuary/skin.estuary_Borsi/g;s/Estuary/Estuary_Borsi/g; s/phil65, Ichabod Fletchman/PiMaker(Hollos)/' \
+                ${STORAGE_DIR}/.kodi/addons/skin.estuary_Borsi/addon.xml
+        else
+            echo ":: Squasfs didn't extracted !!!"
+        fi
+    else
+        echo ":: SkinHack already done!!"
+    fi
 }
 
 scriptAddon() {
@@ -230,10 +249,16 @@ EOF
 }
 
 cleanExit() {
+    ${SUDO} killall dnsmasq
+    if [ -d /mnt/sqfs ];then
+        sudo umount -lv /mnt/sqfs
+        sudo rm -r /mnt/sqfs
+    fi
     # remove this script's nfs shares (lines with #libreELEC) 
     ${SUDO} sed -i '/libreELEC/d' /etc/exports
     # restart nfs server - TODO restore original sttate
-    ${SUDO} exportfs -r
+    ${SUDO} service nfs-kernel-server restart
+    # exportfs -r
 
     ${SUDO} sed -i '/nfsboot.LE/d' ${TFTP_DIR}/config.txt
     # unmount mounted img
@@ -245,17 +270,32 @@ cleanExit() {
     exit 0
 }
 
+rotateDisplay() {
+    echo "video=HDMI-A-1:1920x1080M@60,margin_left=0,margin_right=0,margin_top=0,margin_bottom=0,rotate=180,reflect_x TO THE CMDLINE"
+    # echo "disable_fw_kms_setup=0 TO THE config.txt or in distroconfig.txt"
+}
+
+fs-resize() {
+    DISK=/dev/mmcblk0
+    umount ${DISK}p2 || (echo "NOT RESIZED" && sleep 5 && exit )
+    parted -s -m ${DISK} resizepart 2 100%
+    e2fsck -f -p ${DISK}p2
+    resize2fs ${DISK}p2
+}
+
+
 runLEnfsBoot() {
     trap 'echo "SIGINT !!!" && cleanExit ' INT
     get_img
     # resizeImage
     mountLE
-    LEversion
-    playStartUpVideo
-    disableSplash
-    wizzard
-    createSshKey
-    prepare
+    #LEversion
+    #playStartUpVideo
+    #disableSplash
+    #wizzard
+    #createSshKey
+    #skinHack
+    netBoot
     cleanExit
 }
 
@@ -275,11 +315,13 @@ commands() {
     kodi-send --action="DialogOK(msg="oooooo",100)"
     kodi-send --action="RunScript('/storage/.kodi/myScripts/Animatics.py')"
     xbmc.log( msg='This is a test string.', level=xbmc.LOGDEBUG)
+    $INFO[Player.Title]
+    $INFO[infolabel] 
+
 }
 
 others() {
-   echo # PWM2.py
-# Set RGB color
+   # Set RGB color
 
 import sys
 sys.path.append('/storage/.kodi/addons/virtual.rpi-tools/lib')
