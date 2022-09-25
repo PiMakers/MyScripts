88b1e3333cc528fa69accde6b048c5641067cf65
PiMakers <pimaker@gmail.com>
2022-09-25 09:56:28 +0200

    UPD

--- Works/2022.09.12_Muemlek150/Video_control_app/vigado.sh
+++ Works/2022.09.12_Muemlek150/Video_control_app/vigado.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
+###################################
+# A táphoz közelebbi a touchscreen.
+# A scriptben az útvonalakat cseréld ki a megfelelőekre.
+# A touch input id-ját megtalálja, de csekkold le terminálban:
+# xinput --list | grep -F ILITEK | grep -Po '(?<=id=)\d\d?' | head -n 1
+###################################
+
+
+pathToVideo=~/Videos/vid_FHD_H264.mp4
+pathToApp=~/Documents/Video_control_app
+
 one=$(xinput --list | grep -F ILITEK | grep -Po '(?<=id=)\d\d?' | head -n 1)
 two=$(xinput --list | grep -F ILITEK | grep -Po '(?<=id=)\d\d?' | tail -n 1)
 
 xinput map-to-output $one HDMI-2
 xinput map-to-output $two HDMI-2
 
-cd ~/Documents/Video_control_app && npm start &
-
-cd ~/Videos && mpv vid_FHD_H264.mp4 -screen=1 --fs --idle --loop --no-osc --no-audio --input-ipc-server=/tmp/mpvsocket &
+# cd ~/Documents/Video_control_app && npm start &
+# cd ~/Videos && mpv vid_FHD_H264.mp4 -screen=1 --fs --idle --loop --no-osc --no-audio --input-ipc-server=/tmp/mpvsocket &
+mpv $pathToVideo -screen=1 --fs --idle --loop --no-osc --no-audio --input-ipc-server=/tmp/mpvsocket &
+npm start --prefix=$pathToApp
