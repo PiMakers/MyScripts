03029acfe67a16dad700329b83b1c07be06876c4
pimakers <pimaker@gmail.com>
2022-12-10 09:52:17 +0100

    various

--- Works/2022.09.12_Muemlek150/Video_control_app/renderer.js
+++ Works/2022.09.12_Muemlek150/Video_control_app/renderer.js
@@ -1,19 +1,36 @@
-const { spawn } = require('child_process');
-const fs = require('fs');
+//const { spawn } = require('child_process');
+//const fs = require('fs');
 
-let frames;
+//let frames;
 
-frames = fs.readFileSync('src/frames.txt', 'utf-8').split('\n');
+//frames = fs.readFileSync('src/frames.txt', 'utf-8').split('\n');
+let frames = [ "0", "1250", "1550", "2550","3250","3500","4125","4875","5550","6175","6675","7175","7425","7675","8425"];
+
+function send(command, ...args) {
+  console.log(`Sending command: ${command} params: ${args}`);
+  //if (useNotifications()) {
+  //  audioLoad();
+  //}
+  const path = ['api', command, ...args].join('/');
+
+  const request = new XMLHttpRequest();
+  request.open("post", path);
+
+  request.send(null);
+  // Kick off a refresh quickly in response to user input.
+  //refreshStatus(100);
+}
+
 
 const seekToFrame = (frame) => {
 
-  const sender = spawn('echo', ['{ "command": ["set_property", "time-pos", ' + frame / 25 + '] }']);
+  //const sender = spawn('echo', ['{ "command": ["set_property", "time-pos", ' + frame / 25 + '] }']);
 
-  const socat = spawn('socat', ['-', '/tmp/mpvsocket'])
+  //const socat = spawn('socat', ['-', '/tmp/mpvsocket'])
 
-  sender.stdout.on('data', data => {
-    socat.stdin.write(data);
-  })
+  //sender.stdout.on('data', data => {
+    //socat.stdin.write(data);
+  //})
 
   // socat.stdout.on('data', (data) => {
   //   console.log(data.toString());
@@ -28,12 +45,13 @@ buttons.forEach(el => {
     el.childNodes[1].classList.add("playing")
     const serial = el.dataset.serial;
     const idx = serial - 1;
-    const frame = frames[idx];
+    const frame = frames[idx]/25;
     if (frame == undefined) {
       throw `${idx} is out of bounds!`;
     }
     console.log(`Seeked to frame ${frame}`)
-    seekToFrame(frame)
+    send('set_position',frame)
+    //seekToFrame(frame)
     setTimeout(() => {
       el.childNodes[1].classList.remove("playing")
     }, 10000);
